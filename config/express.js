4a21199456ec4c47a146dcb2d951d9c849709194
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-09-03 17:30:47 -0600

    remove msoca

--- config/express.js
+++ config/express.js
@@ -1,87 +1,55 @@
 import express from 'express';
-import mosca from 'mosca';
-import server from 'http';
-import socket from 'socket.io';
 import UUID from 'uuid';
+import socket from 'socket.io';
+import mqtt from 'mqtt';
 import r from 'rethinkdb';
 import logger from './winston';
 import config from './config';
 
-const ioServer = server.createServer();
-const io = socket(ioServer);
 
-io.on('connection', (client) => {
-  client.userid = UUID(); //eslint-disable-line
-  logger.log('info', 'socket.io:: player %s connected', client.userid);
-
-  client.on('event', (data) => {
-    client.emit('send_event', data);
-  });
-  client.on('disconnect', () => {
-
-  });
-});
-
-ioServer.listen(config.socketPort);
-
+const io = socket.listen(config.socketPort);
+const debug = true;
 
 let connection = null;
-r.connect({ host: '45.55.162.243', port: 28015 }, (err, conn) => {
+r.connect({ host: '45.55.162.243', port: 28015, user: 'admin', password: '8V6-Zco-5ux-EMu' }, (err, conn) => {
   if (err) throw err;
   connection = conn;
   connection.use('iot');
+  console.log('connected to rethinkdb');
 });
 
-const ascoltatore = {
-  // using ascoltatore
-  type: 'mongo',
-  url: config.mqtt,
-  pubsubCollection: 'ascoltatori',
-  mongo: {}
-};
 
-const settings = {
-  port: 1884,
-  backend: ascoltatore
-};
+const broker = mqtt.connect('ws://45.55.162.243:8083');
 
-const moscaServer = new mosca.Server(settings);
-
-moscaServer.on('clientConnected', (client) => {
-  logger.log('info', 'client connected', client.id);
+io.sockets.on('connection', (client) => {
+  client.uuid = UUID(); //eslint-disable-line
+  if (debug) {
+    console.log('web client connected', client.uuid); //eslint-disable-line
+  }
+  client.on('disconnect', () => {
+  });
 });
 
-// fired when a message is received
-moscaServer.on('published', (packet, client) => {
-  const topic = packet.topic;
-  switch (topic) {
-    case 'device/connected':
-      return handleDeviceConnected(packet, client);
-    case 'device/state':
-      return handleDeviceState(packet, client);
-    default:
-      return undefined;
+
+broker.on('connect', () => {
+  console.log('connection');
+  broker.subscribe('connected/device', { qos: 2 });
+});
+broker.on('message', (topic, message) => {
+  console.log(topic);
+  console.log(message.toString());
+  if (topic === 'connected/device') {
+    const arrayInsert = JSON.parse(message.toString());
+    const insertData = {
+      ...arrayInsert,
+      date: new Date()
+    };
+    r.table('devices').insert(insertData).run(connection, (err) => {
+      if (err) throw logger.log('error', 'error inserting data %s', err);
+    });
+    io.sockets.emit('send/device', insertData);
   }
 });
-
-moscaServer.on('ready', () => { logger.log('info', 'Mosca is running'); });
-
-function handleDeviceConnected(message, client) {
-  const arrayInsert = JSON.parse(message.payload.toString());
-  const insertData = {
-    ...arrayInsert,
-    date: new Date()
-  };
-  r.table('devices').insert(insertData).run(connection, (err) => {
-    if (err) throw err;
-  });
-  moscaServer.publish({ topic: 'connected/device', payload: JSON.stringify(message) }, client);
-}
-
-function handleDeviceState(message, client) {
-  logger.log('info', 'device state update to %s', message.payload);
-  moscaServer.publish({ topic: 'state/device', payload: 'data payload' }, client);
-}
 
 const app = express();
 export default app;
--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 // config should be imported before importing any other file
 import polyfill from 'babel-polyfill'; // eslint-disable-line
-import mongoose from 'mongoose';
 import pmx from 'pmx';                 // eslint-disable-line
 pmx.init({ http: true }); // eslint-disable-line enable http keymetris
 import config from './config/config';  // eslint-disable-line
 import app from './config/express';    // eslint-disable-line
 
-mongoose.connect(config.mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
-mongoose.Promise = global.Promise; // Tell mongoose to use es6 promises
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`);
-});
 
 export default app;
